0ed029d48f1281ee736833b3cd11a451522690f9
Michael Frister <michael+git@frister.net>
2012-10-28 12:38:09 +0000

    Allow devices to create graph connections

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ var http = require('http');
 var socketio = require('socket.io');
 
 var Device = require('./lib/device');
-
+var Graph = require('./lib/graph');
 
 var app = express();
 var server = http.createServer(app);
@@ -15,6 +15,8 @@ app.use('/', express['static'](__dirname + '/static'));
 
 // Web Socket Server
 var devices = {};
+var graph = new Graph();
+var pendingGraphSource;
 var ids = 1;
 io.sockets.on('connection', function (socket) {
     var device = new Device(ids++);
@@ -23,9 +25,25 @@ io.sockets.on('connection', function (socket) {
     socket.on('orientation', function(data) { device.emit('orientation', data); });
     socket.on('motion', function(data) { device.emit('motion', data); });
     socket.on('rewire', function(data) {
-        console.warn('rewiring', device.id);
     });
     socket.on('disconnect', function() { delete devices[device.id]; });
+    socket.on('disconnect', function() {
+        graph.deleteNode(device.id);
+        delete devices[device.id];
+    });
+    socket.on('rewire', function() {
+        console.warn('rewiring', device.id);
+        if(!pendingGraphSource || pendingGraphSource == device.id) {
+            pendingGraphSource = device.id;
+            console.info('graph source: ' + device.id.toString());
+        } else {
+            graph.deleteNode(pendingGraphSource);
+            graph.deleteNode(device.id);
+            graph.push([pendingGraphSource, device.id]);
+            console.info('graph connection: ' + pendingGraphSource + ' -> ' + device.id.toString());
+            pendingGraphSource = null;
+        }
+    });
 });
 
 server.listen(8000);
